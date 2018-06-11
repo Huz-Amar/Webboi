9b417b165fbb6a4e9508fb3e6d18e3584d9b3e5c
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-06-11 17:39:18 -0600

    sdf

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/homepage.html'));
    //res.send('Hello GET');
 })
 
