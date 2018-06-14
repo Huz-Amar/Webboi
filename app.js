fbb19dc03c203f1ef2dca4261bb8f14273900777
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-06-14 00:39:09 -0600

    so many changes holy shit

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var port = 3000;
 
+//using ejs and express Layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
 var router = require('./app/routes');
 app.use('/', router);
-
-//using ejs and express Layouts
-
 
 //static file locations
 app.use(express.static(__dirname + '/public'));
---
+++ views/pages/codeForInventory.js
@@ -0,0 +1,5 @@
+function displayText(){
+    cosole.log("Reaching external js file");
+    alert("Nigga pls work");
+    //buggy boi
+}
