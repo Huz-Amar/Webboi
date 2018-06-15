0941314def297e5a1cdec3a96122c5e85aed4dd6
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-06-15 17:24:26 -0600

    g

--- public/js/codeForInventory.js
+++ public/js/codeForInventory.js
@@ -1,16 +1,44 @@
 
 function displayText(){
-    console.log("Reaching external js file");
-    alert("Nigga pls work");
+    
 }
 
 function newEntry(){
     var txt;
     var person = prompt("Clearence Level", "");
+    var shame = prompt("This work?", "");
     if (person == null || person == "") {
         txt = "User cancelled the prompt.";
     } 
     else {
         txt = "Hello " + person + "! How are you today?";
     }
+}
+
+function extractInfo(){
+    var mysql = require('mysql');
+var path = require('path');
+var sqlCredentials = require(path.join(__dirname, '../../credentials.json'));
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: sqlCredentials.username,
+    password: sqlCredentials.password,
+    database: 'InventoryDB',
+});
+
+connection.connect();
+
+// var tool = {
+//     ID: '2000',
+//     TOOLNAME: 'HORMUZ ORMUZ',
+//     QUANTITY: '500',
+//     PRICE: '11.45',
+//     SUPPLIERID: '9000'
+// };
+
+var sql = "SELECT * FROM ToolTable WHERE ID=3000";
+connection.query(sql, function (err, result){
+    console.log(JSON.stringify(result[0].TOOLNAME));
+});
 }
--- public/js/demo_db_connection.js
+++ public/js/demo_db_connection.js
@@ -19,7 +19,8 @@ connection.connect();
 //     SUPPLIERID: '9000'
 // };
 
-var sql = "INSERT INTO ToolTable (ID, TOOLNAME, QUANTITY, PRICE, SUPPLIERID) VALUES ('3001', 'HORMUZ ORMUZ', '500', '11.45', '9000')";
-connection.query(sql, function (err, result) {
-    console.log("1 record inserted");
+var sql = "SELECT * FROM ToolTable WHERE ID=3000";
+connection.query(sql, function (err, result){
+    console.log(JSON.stringify(result[0].TOOLNAME));
+    
 });
