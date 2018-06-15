35c938898a9e70642037fc55b799bcf369cc93bb
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-06-15 13:35:11 -0600

    js external now working

--- views/pages/demo_create_db.js
+++ views/pages/demo_create_db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "huzaifa147"
+  password: "password"
 });
 
 con.connect(function(err) {
