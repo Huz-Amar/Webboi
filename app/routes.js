fd9e4d034a0d1c840b0e65fc0f89b335907f8ce4
DANMAN\DanGe <daniel.guieb2@gmail.com>
2018-06-22 14:03:16 -0600

    wow big think

--- app/routes.js
+++ app/routes.js
@@ -5,33 +5,53 @@ var router = express.Router();
 
 module.exports= router;
 
+
 router.get('/', function(req, res) {
 	res.render('pages/index');
 })
+
+
 
 router.get('/supplier_info', function(req, res) {
 	res.render('pages/supplier_info');
 })
 
+
+
 router.get('/inventory', function(req, res) {
 	res.render('pages/inventory');
 })
+
+
 
 router.get('/customer_history', function(req, res) {
 	res.render('pages/customer_history');
 })
 
+
+
 router.get('/client_list', function(req, res) {
 	res.render('pages/client_list');
 })
 
+
+// temp wip stuff
+// when doing this for the actual database connection, should use a function to get resultSet 
+// and then use that result set as
+// const empTable = resultSet (???)
+const empTable = require('../public/js/listOfEmployees.json');
+
 router.get('/employees', function(req, res) {
-	res.render('pages/employees');
+	res.render('pages/employees', {employees: empTable});
 })
+
+
 
 router.get('/prescription_tickets', function(req, res) {
 	res.render('pages/prescription_tickets');
 })
+
+
 
 router.get('/login', function(req, res) {
 	res.render('pages/login');
