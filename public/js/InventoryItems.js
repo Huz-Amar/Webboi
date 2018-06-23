60e440c183e8c222073b60efe954cbddd8730627
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-06-22 20:51:07 -0600

    m

--- public/js/InventoryItems.js
+++ public/js/InventoryItems.js
@@ -12,21 +12,19 @@ var connection = mysql.createConnection({
 connection.connect();
 
 //exports.getInfo = function(req, res){
-    var sql = "SELECT * FROM pharmaceutical, inventory_item WHERE CLEARENCE_LEVEL='B' AND STOCK_ID=PHARM_ID";
-    connection.query(sql, function (err, result){
+var sql = "SELECT * FROM inventory_item WHERE CLEARENCE_LEVEL='C' ";
+connection.query(sql, function (err, result){
     //console.log(JSON.stringify(result[0].TOOLNAME));
     var fs = require('fs');
     var stringboi = "[\n"
     var lasti = result.length-1;
     for (var i=0; i<result.length-1; i++){
-        stringboi += JSON.stringify({name: result[i].NAME, expiration_date: result[i].EXPIRATION_DATE, 
-            description: result[i].DESCRIPTION, amount: result[i].AMOUNT}, null, "\t");
+        stringboi += JSON.stringify({Stock_ID: result[i].STOCK_ID, Supp_Name: result[i].SUPP_NAME, Inventory_Type: result[i].TYPE, Amount: result[i].Amount}, null, "\t");
         stringboi += ",\n";
     }
-    stringboi += JSON.stringify({name: result[lasti].NAME, expiration_date: result[lasti].EXPIRATION_DATE, 
-        description: result[lasti].DESCRIPTION, amount: result[i].AMOUNT}, null, "\t");
+    stringboi += JSON.stringify({Stock_ID: result[lasti].STOCK_ID, Supp_Name: result[lasti].SUPP_NAME, Inventory_Type: result[lasti].TYPE, Amount: result[lasti].Amount}, null, "\t");
     stringboi += "]";
-    fs.writeFile("extractInfoInventoryB.json", stringboi, finished);
+    fs.writeFile("InventoryItem.json", stringboi, finished);
     function finished(err){
         console.log("all set");
     }
